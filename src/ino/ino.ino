45341be7bf099f0f779b9625fe79a4ccb982d909
David Weber <dsw7@sfu.ca>
2022-12-24 03:42:01 -0800

    Add basic echo

--- src/ino/ino.ino
+++ src/ino/ino.ino
@@ -1,12 +1,29 @@
+const unsigned int MAX_SIZE_MSG = 12;
+
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);                       // wait for a second
+    while (Serial.available() > 0)
+    {
+        static char msg[MAX_SIZE_MSG];
+        static unsigned int idx = 0;
+
+        char inc = Serial.read();
+
+        if ((inc != '\n') and (idx < MAX_SIZE_MSG - 1))
+        {
+            msg[idx] = inc;
+            idx++;
+        }
+        else
+        {
+            msg[idx] = '\0';
+            Serial.println(msg);
+            idx = 0;
+        }
+    }
 }
