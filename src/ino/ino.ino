bb77b357c7820776c311441ffc448e06650182ea
David Weber <dsw7@sfu.ca>
2022-12-25 05:22:27 -0800

    Add global namespace resolution

--- src/ino/ino.ino
+++ src/ino/ino.ino
@@ -6,45 +6,45 @@ namespace Core
 
 void run_connection_test()
 {
-    Serial.println("Hello from InoDAQ2. I should blink 5 times!");
-    Serial.flush();
+    ::Serial.println("Hello from InoDAQ2. I should blink 5 times!");
+    ::Serial.flush();
 
     for (unsigned int i = 0; i < 5; ++i)
     {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
+        ::digitalWrite(LED_BUILTIN, HIGH);
+        ::delay(100);
+        ::digitalWrite(LED_BUILTIN, LOW);
+        ::delay(100);
     }
 }
 
 void exit_program()
 {
-    Serial.println("Closing connection. Goodbye!");
-    Serial.flush();
-    Serial.end();
+    ::Serial.println("Closing connection. Goodbye!");
+    ::Serial.flush();
+    ::Serial.end();
 }
 
 void handle_unknown_command(const String &command)
 {
-    Serial.println("Unknown command: " + command);
-    Serial.flush();
+    ::Serial.println("Unknown command: " + command);
+    ::Serial.flush();
 }
 
 } // namespace Core
 
 void setup()
 {
-    Serial.begin(::BAUD_RATE);
-    Serial.setTimeout(::MAX_TIME_MILLISEC_WAIT_SERIAL_DATA);
-    pinMode(LED_BUILTIN, OUTPUT);
+    ::Serial.begin(::BAUD_RATE);
+    ::Serial.setTimeout(::MAX_TIME_MILLISEC_WAIT_SERIAL_DATA);
+    ::pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-    while (Serial.available() > 0)
+    while (::Serial.available() > 0)
     {
-        String command = Serial.readString();
+        String command = ::Serial.readString();
         command.trim();
 
         if (command == "hello")
