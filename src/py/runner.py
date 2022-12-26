f0debc96fb4274da69205d56458fc0345e4711ed
David Weber <dsw7@sfu.ca>
2022-12-25 19:37:22 -0800

    Improve CLI

--- src/py/runner.py
+++ src/py/runner.py
@@ -3,25 +3,28 @@ import click
 from serial_connection import SerialConnection
 
 @click.group()
-def main() -> None:
-    pass
+@click.option('-p', '--port', default='/dev/ttyS2', help='Specify USB port', metavar='<com-port>')
+@click.pass_context
+def main(context: click.core.Context, port: str) -> None:
+    context.obj = {
+        'port': port
+    }
 
-@main.command()
-@click.option('-p', '--port')
-def hello(**configs: Dict[str, str]) -> None:
+@main.command(help='Handshake with device')
+@click.pass_obj
+def hello(obj: Dict[str, str]) -> None:
 
-    with SerialConnection(serial_port=configs['port']) as connection:
+    with SerialConnection(serial_port=obj['port']) as connection:
         connection.send_message(b'hello')
         connection.receive_message()
 
-@main.command()
-@click.option('-p', '--port')
-@click.option('-n', '--count')
-def read(**configs: Dict[str, str]) -> None:
+@main.command(help='Collect a trace')
+@click.option('-n', '--count', default=5, help='Specify number of reads', metavar='<num-reads>')
+@click.pass_obj
+def read(obj: Dict[str, str], count: str) -> None:
 
-    with SerialConnection(serial_port=configs['port']) as connection:
-        command = f"read:{configs['count']}".encode()
-        connection.send_message(command)
+    with SerialConnection(serial_port=obj['port']) as connection:
+        connection.send_message(f'read:{count}'.encode())
         connection.receive_message()
 
 if __name__ == '__main__':
