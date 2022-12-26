093d1e109a05739d272fdb260b3c0bc869e2aed3
David Weber <dsw7@sfu.ca>
2022-12-25 21:00:19 -0800

    Add a timer

--- src/py/runner.py
+++ src/py/runner.py
@@ -1,4 +1,5 @@
 from typing import Dict, Union
+from time import perf_counter_ns
 import click
 from serial_connection import SerialConnection
 
@@ -12,6 +13,7 @@ def main(context: click.core.Context, **configs: Dict[str, Union[bool, str]]) ->
 @main.command(help='Handshake with device')
 @click.pass_obj
 def hello(obj: Dict[str, Union[bool, str]]) -> None:
+    click.secho('> Handshaking with device', fg='yellow')
 
     with SerialConnection(**obj) as connection:
         connection.send_message(b'hello')
@@ -21,10 +23,15 @@ def hello(obj: Dict[str, Union[bool, str]]) -> None:
 @click.option('-n', '--count', default=5, help='Specify number of reads', metavar='<num-reads>')
 @click.pass_obj
 def read(obj: Dict[str, Union[bool, str]], count: str) -> None:
+    click.secho('> Reading data from device', fg='yellow')
 
     with SerialConnection(**obj) as connection:
+        start = perf_counter_ns()
         connection.send_message(f'read:{count}'.encode())
         connection.receive_message()
+
+        end = (perf_counter_ns() - start) / 1_000_000
+        click.secho(f'> Round trip time: {end} ms', fg='yellow')
 
 if __name__ == '__main__':
     main()
