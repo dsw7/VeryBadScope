1b66ba8bfc8ba94a4433ac3125fa6fbe73e4ce23
David Weber <dsw7@sfu.ca>
2022-12-25 20:14:18 -0800

    Misc cleanup

--- src/py/runner.py
+++ src/py/runner.py
@@ -1,29 +1,28 @@
-from typing import Dict
+from typing import Dict, Union
 import click
 from serial_connection import SerialConnection
 
 @click.group()
 @click.option('-p', '--port', default='/dev/ttyS2', help='Specify USB port', metavar='<com-port>')
+@click.option('-d', '--debug', is_flag=True, help='Enable debug logging')
 @click.pass_context
-def main(context: click.core.Context, port: str) -> None:
-    context.obj = {
-        'port': port
-    }
+def main(context: click.core.Context, **configs: Dict[str, Union[bool, str]]) -> None:
+    context.obj = configs
 
 @main.command(help='Handshake with device')
 @click.pass_obj
-def hello(obj: Dict[str, str]) -> None:
+def hello(obj: Dict[str, Union[bool, str]]) -> None:
 
-    with SerialConnection(serial_port=obj['port']) as connection:
+    with SerialConnection(**obj) as connection:
         connection.send_message(b'hello')
         connection.receive_message()
 
 @main.command(help='Collect a trace')
 @click.option('-n', '--count', default=5, help='Specify number of reads', metavar='<num-reads>')
 @click.pass_obj
-def read(obj: Dict[str, str], count: str) -> None:
+def read(obj: Dict[str, Union[bool, str]], count: str) -> None:
 
-    with SerialConnection(serial_port=obj['port']) as connection:
+    with SerialConnection(**obj) as connection:
         connection.send_message(f'read:{count}'.encode())
         connection.receive_message()
 
--- src/py/serial_connection.py
+++ src/py/serial_connection.py
@@ -5,18 +5,30 @@ from json import dumps
 from typing import TypeVar
 import serial
 
-logging.basicConfig(level=logging.DEBUG)
-
 BAUD_RATE = 9600
 T = TypeVar('T')
 
 
 class SerialConnection:
 
-    def __init__(self: T, serial_port: str) -> T:
+    def __init__(self: T, port: str, debug: bool) -> T:
 
         self.serial_port_obj = None
-        self.serial_port = serial_port
+        self.port = port
+
+        if debug:
+            level = logging.DEBUG
+        else:
+            level = logging.INFO
+
+        configs = {
+            'level': level,
+            'format': '%(asctime)s %(message)s',
+            'datefmt': '%Y-%m-%d %I:%M:%S'
+        }
+
+        logging.basicConfig(**configs)
+        self.logger = logging.getLogger(__class__.__name__)
 
     def __enter__(self: T) -> T:
 
@@ -26,10 +38,10 @@ class SerialConnection:
             'stopbits': serial.STOPBITS_ONE,
             'bytesize': serial.EIGHTBITS,
             'timeout': 5,
-            'port': self.serial_port
+            'port': self.port
         }
 
-        logging.debug('Connecting using parameters: %s', dumps(connection_params, indent=4))
+        self.logger.debug('Connecting using parameters: %s', dumps(connection_params, indent=4))
 
         try:
             self.serial_port_obj = serial.Serial(**connection_params)
@@ -42,7 +54,7 @@ class SerialConnection:
         # Opening a connection will send a DTR (Data Terminal Ready) signal to device, which will
         # force the device to reset. Give device 2 seconds to reset
 
-        logging.debug('DTR (Data Terminal Ready) was sent. Waiting for device to reset')
+        self.logger.debug('DTR (Data Terminal Ready) was sent. Waiting for device to reset')
         sleep(2)
 
         return self
@@ -50,10 +62,10 @@ class SerialConnection:
     def __exit__(self: T, *args) -> None:
 
         if self.serial_port_obj is None:
-            logging.debug('Not closing connection. Connection was never opened!')
+            self.logger.debug('Not closing connection. Connection was never opened!')
             return
 
-        logging.debug('Closing connection!')
+        self.logger.debug('Closing connection!')
 
         if self.serial_port_obj.is_open:
 
@@ -63,12 +75,12 @@ class SerialConnection:
 
     def send_message(self: T, message: bytes) -> None:
 
-        logging.debug('Sending message: "%s"', message)
-        logging.debug('Sent %i bytes', self.serial_port_obj.write(message))
+        self.logger.debug('Sending message: "%s"', message)
+        self.logger.debug('Sent %i bytes', self.serial_port_obj.write(message))
 
     def receive_message(self: T) -> bytes:
 
-        logging.debug('Waiting to receive message...')
+        self.logger.debug('Waiting to receive message...')
         message_received = False
 
         while not message_received:
@@ -79,5 +91,5 @@ class SerialConnection:
             bytes_from_dev = self.serial_port_obj.read_until()  # Reads until \n by default
             message_received = True
 
-        logging.debug('Received message: %s', bytes_from_dev)
+        self.logger.debug('Received message: %s', bytes_from_dev)
         return bytes_from_dev
