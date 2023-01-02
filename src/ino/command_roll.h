8749dafa39e8a2eb77584d58e031f1adc0bd4501
David Weber <dsw7@sfu.ca>
2023-01-01 22:04:05 -0800

    Separate into classes

--- src/ino/command_roll.h
+++ src/ino/command_roll.h
@@ -1,32 +1,9 @@
 #pragma once
 
-#include "Arduino.h"
+#include "command_scope_base.h"
 
 namespace Command
 {
-    class ScopeBase
-    {
-        public:
-            void set_command(const ::String &command);
-            virtual void acquire_data() = 0;
-
-        protected:
-            ::String command = "read:5:1000";
-
-            int idx_record_length = 0;
-            int idx_measurement_duration = 0;
-
-            long record_length = 0;
-            long measurement_duration = 0;
-            long period = 0;
-
-            bool validate_command();
-            bool parse_command_indices();
-            bool parse_record_length();
-            bool parse_measurement_duration();
-            bool compute_period();
-    };
-
     class Roll: public ScopeBase
     {
         public:
