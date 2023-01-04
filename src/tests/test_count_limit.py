0382b96b0fdc1e46ec37cf22c6a2896de5d45ec6
David Weber <dsw7@sfu.ca>
2023-01-04 01:54:03 -0800

    Add upper limit test

--- src/tests/test_count_limit.py
+++ src/tests/test_count_limit.py
@@ -9,11 +9,3 @@ def test_265_count(connection: SerialConnection) -> None:
 
     rv, _ = connection.receive_message()  # times
     assert rv
-
-def test_280_count(connection: SerialConnection) -> None:
-
-    connection.send_message('roll:280:1000000')
-    rv, error = connection.receive_message()
-
-    assert not rv
-    assert 'An exception occurred when decoding results:' in error
