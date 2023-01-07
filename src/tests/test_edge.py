b4b9dc0bf5d531e20d6896e7b7f5513b0cd1e2d5
David Weber <dsw7@sfu.ca>
2023-01-07 02:35:05 -0800

    Use test parametrization

--- src/tests/test_edge.py
+++ src/tests/test_edge.py
@@ -1,73 +1,23 @@
+from pytest import mark
 from src.py.serial_connection import SerialConnection
 
-def test_edge_1(connection: SerialConnection) -> None:
+PAIRS_EDGE = [
+    ('edge:', 'Malformed command! Missing one or more colons'),
+    ('edge:5', 'Malformed command! Missing one or more colons'),
+    ('edge:5:1000', 'Malformed command! Missing one or more colons'),
+    ('edge:5:1000:rising', 'Malformed command! Missing one or more colons'),
+    ('edge:5:1000:foobar:4', 'Invalid trigger type!'),
+    ('edge:5:1000::4', 'Could not parse trigger type!'),
+    ('edge:5:1000:rising:', 'Could not parse trigger delta!'),
+    ('edge:5:1000:rising:19', 'Trigger delta must be at least 0.1 volts!'),
+    ('edge:5:1000:rising:1024', 'Trigger delta cannot exceed 5 volts!')
+]
 
-    connection.send_message('edge:')
+@mark.parametrize('command, error', PAIRS_EDGE)
+def test_edge(connection: SerialConnection, command: str, error: str) -> None:
+
+    connection.send_message(command)
     rv, message = connection.receive_message()
 
     assert not rv
-    assert message == 'Malformed command! Missing one or more colons'
-
-def test_edge_2(connection: SerialConnection) -> None:
-
-    connection.send_message('edge:5')
-    rv, message = connection.receive_message()
-
-    assert not rv
-    assert message == 'Malformed command! Missing one or more colons'
-
-def test_edge_3(connection: SerialConnection) -> None:
-
-    connection.send_message('edge:5:1000')
-    rv, message = connection.receive_message()
-
-    assert not rv
-    assert message == 'Malformed command! Missing one or more colons'
-
-def test_edge_4(connection: SerialConnection) -> None:
-
-    connection.send_message('edge:5:1000:rising')
-    rv, message = connection.receive_message()
-
-    assert not rv
-    assert message == 'Malformed command! Missing one or more colons'
-
-def test_edge_5(connection: SerialConnection) -> None:
-
-    connection.send_message('edge:5:1000:foobar:4')
-    rv, message = connection.receive_message()
-
-    assert not rv
-    assert message == 'Invalid trigger type!'
-
-def test_edge_6(connection: SerialConnection) -> None:
-
-    connection.send_message('edge:5:1000::4')
-    rv, message = connection.receive_message()
-
-    assert not rv
-    assert message == 'Could not parse trigger type!'
-
-def test_edge_7(connection: SerialConnection) -> None:
-
-    connection.send_message('edge:5:1000:rising:')
-    rv, message = connection.receive_message()
-
-    assert not rv
-    assert message == 'Could not parse trigger delta!'
-
-def test_edge_8(connection: SerialConnection) -> None:
-
-    connection.send_message('edge:5:1000:rising:19')
-    rv, message = connection.receive_message()
-
-    assert not rv
-    assert message == 'Trigger delta must be at least 0.1 volts!'
-
-def test_edge_9(connection: SerialConnection) -> None:
-
-    connection.send_message('edge:5:1000:rising:1024')
-    rv, message = connection.receive_message()
-
-    assert not rv
-    assert message == 'Trigger delta cannot exceed 5 volts!'
+    assert message == error
