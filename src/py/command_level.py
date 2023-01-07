5aafefcee0a9e3f28b9a65fda73ad76e1eaab592
David Weber <dsw7@sfu.ca>
2023-01-07 04:40:13 -0800

    Add level CLI

---
+++ src/py/command_level.py
@@ -0,0 +1,33 @@
+from time import perf_counter_ns
+import sys
+from click import secho
+from serial_connection import SerialConnection
+from command_base import CommandBase, T, volt_to_analog
+
+
+class CommandLevelTrigger(CommandBase):
+
+    def acquire_data(self: T) -> None:
+
+        secho('> Waiting on trigger...', fg='yellow')
+
+        record_length = self.cli_params['record_length']
+        measurement_duration = self.cli_params['measurement_duration']
+        trigger = self.cli_params['trigger']
+        level = volt_to_analog(self.cli_params['level'])
+
+        with SerialConnection(**self.cli_params) as connection:
+
+            start = perf_counter_ns()
+            connection.send_message(f'level:{record_length}:{measurement_duration}:{trigger}:{level}')
+
+            rv, self.raw_v = connection.receive_message()
+            if not rv:
+                sys.exit(self.raw_v)
+
+            rv, self.raw_t = connection.receive_message()
+            if not rv:
+                sys.exit(self.raw_t)
+
+            end = (perf_counter_ns() - start) / 1_000_000
+            secho(f'> Round trip time: {end} ms', fg='yellow')
--- src/py/runner.py
+++ src/py/runner.py
@@ -3,6 +3,7 @@ import click
 from command_hello import command_hello
 from command_roll import CommandRoll
 from command_edge import CommandEdgeTrigger
+from command_level import CommandLevelTrigger
 
 @click.group()
 @click.option('-p', '--port', default='/dev/ttyS2', help='Specify USB port', metavar='<com-port>')
@@ -39,6 +40,19 @@ def roll(obj: Dict[str, Union[bool, str]], **options: Dict[str, str]) -> None:
 @click.pass_obj
 def edge(obj: Dict[str, Union[bool, str]], **options: Dict[str, str]) -> None:
     CommandEdgeTrigger(**obj, **options).main()
+
+@main.command(help='Collect a trace with rising- or falling edge triggering at a specific level')
+@click.option('-n', '--record-length', default=5, help='Specify number of reads after the trigger', metavar='<num-reads>')
+@click.option('-r', '--measurement-duration', default=1000, help='Specify time range to read over', metavar='<microseconds>')
+@click.option('-t', '--trigger', type=click.Choice(['rising', 'falling']), default='rising', help='Specify trigger type')
+@click.option('-l', '--level', default=4, help='Specify level at which to trigger', metavar='<voltage>', type=float)
+@click.option('-p', '--plot', is_flag=True, help='Plot results')
+@click.option('-q', '--plot-path', default='/tmp/edge.png', help='Where to dump plot if --plot is specified', metavar='<path>')
+@click.option('-t', '--to-csv', is_flag=True, help='Export results to CSV')
+@click.option('-u', '--csv-path', default='/tmp/edge.csv', help='Where to dump CSV if --to-csv is specified', metavar='<path>')
+@click.pass_obj
+def level(obj: Dict[str, Union[bool, str]], **options: Dict[str, str]) -> None:
+    CommandLevelTrigger(**obj, **options).main()
 
 if __name__ == '__main__':
     main()
