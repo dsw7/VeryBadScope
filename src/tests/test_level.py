34b856d5a8b5b2ad02215c534784b4d77ae5a51e
David Weber <dsw7@sfu.ca>
2023-01-07 21:11:52 -0800

    Add level tests

--- src/tests/test_level.py
+++ src/tests/test_level.py
@@ -6,11 +6,15 @@ PAIRS_LEVEL = [
     ('level:5', 'Malformed command! Missing one or more colons'),
     ('level:5:1000', 'Malformed command! Missing one or more colons'),
     ('level:5:1000:rising', 'Malformed command! Missing one or more colons'),
-    ('level:5:1000:foobar:4', 'Invalid trigger type!'),
-    ('level:5:1000::4', 'Could not parse trigger type!'),
-    ('level:5:1000:rising:', 'Could not parse trigger level!'),
-    ('level:5:1000:rising:4', 'Trigger level must be at least 0.025 volts!'),
-    ('level:5:1000:rising:1024', 'Trigger level cannot exceed 5 volts!')
+    ('level:5:1000:rising:800', 'Malformed command! Missing one or more colons'),
+    ('level:5:1000:foobar:800:25', 'Invalid trigger type!'),
+    ('level:5:1000::800:25', 'Could not parse trigger type!'),
+    ('level:5:1000:rising::25', 'Could not parse trigger level!'),
+    ('level:5:1000:rising:800:', 'Could not parse epsilon!'),
+    ('level:5:1000:rising:4:25', 'Trigger level must be at least 0.025 volts!'),
+    ('level:5:1000:rising:1024:25', 'Trigger level cannot exceed 5 volts!'),
+    ('level:5:1000:rising:800:5', 'Epsilon must be at least 0.05 volts!'),
+    ('level:5:1000:rising:800:1025', 'Epsilon cannot exceed 5 volts!')
 ]
 
 @mark.parametrize('command, error', PAIRS_LEVEL)
